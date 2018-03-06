cca2c13e9931ab0dd4ec94eb7a8ce9bab3e23390
Gabriel Carrizo <gabriel.carrizo@now.se>
2018-03-06 15:10:34 +0100

    progress

--- g_hopfield.py
+++ g_hopfield.py
@@ -22,13 +22,12 @@ def generateData():
 def generateNoisyData():
     return [[1, -1, 1, -1, 1, -1, -1, 1], [1, 1, -1, -1, -1, 1, -1, -1], [1, 1, 1, -1, 1, 1, -1, 1]]
 
-def compW(X, iter=1):
+def compW(X,N=1024):
     Id = np.eye(len(X[0]))
     W = np.zeros((len(X[0]),len(X[0])))
-    for i in range(iter):
-        for x in X:
-            W += np.outer(x,x)
-    return W-Id
+    for x in X:
+        W += (np.outer(x,x))/N
+    return W
 
 def recallAll(X,W):
     out = []
@@ -61,6 +60,8 @@ def recallAsync(x, W, iter=5):
     x_temp = np.copy(x)
     saved_ims = []
     count = 0
+    E = []
+
     for n in range(iter):
         choices = np.copy(steps)
         for i in steps:
@@ -70,18 +71,18 @@ def recallAsync(x, W, iter=5):
                 saved_ims.append(np.copy(x_temp))
                 count = 0
             count+=1
+            E.append(energy(x_temp, W))
     saved_ims.append(x_temp)
 
-    return saved_ims
+    return saved_ims,E
 
-def plotIms(X, exciteIm, recalledIm, save=0, append="", path=""):
+def plotIms(X, exciteIm, recalledIm, save=0, append="", path="test"):
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=3, metadata=dict(artist='Me'), bitrate=1800)
-    ims = []
     fig, ax = plt.subplots(2,3)
-    with writer.saving(fig, "writer_test.mp4", len(exciteIm)):
-        for i in range(len(exciteIm)):
-
+    print(len(recalledIm))
+    with writer.saving(fig, path, len(exciteIm)):
+        for i in range(len(recalledIm)):
             plt.subplot(2,3,1)
             plt.imshow(X[0].reshape(32,32))
             plt.title("Train img1")
@@ -103,11 +104,19 @@ def plotIms(X, exciteIm, recalledIm, save=0, append="", path=""):
             fig.suptitle("Number of itertions: {}".format(i*100))
 
             writer.grab_frame()
+            print(i)
 
     plt.show()
 
+def energy(x,W):
+    return -np.dot(np.dot(x,W),x.T)
+
+def addNoise(img, noiseLevel):
+    mask = np.random.choice([-1, 1], size=(len(img),), p=[noiseLevel, 1-noiseLevel])
+    return np.multiply(img,mask)
 
 
+#------------------------------------------------------------
 def assignment3_0():
     X = generateData()
     W = compW(X)
@@ -119,7 +128,6 @@ def assignment3_1():
 
     print("X: {}".format(X))
     W = compW(X)
-
     #print(recallAll(X_noisy,W))
 
     print("Number of attractors: {}".format(len(findAttractors(W))))
@@ -130,14 +138,220 @@ def assignment3_2():
 
     exciteIm = X[10]
 
-    recalledIms = recallAsync(exciteIm, W)
+    recalledIms, = recallAsync(exciteIm, W)
 
     plotIms(X, exciteIm, recalledIms, save=1, append="", path="")
 
     #recalledIm = np.array(recall(exciteIm, W))
 
+def assignment3_3():
+    X = loadData('pict.dat') #loads 32*32x11
+    attractor = X[:3]
+    W = compW(attractor, iter=10, N=1024)
+
+    exciteIm = X[10]
+    dummy,E = recallAsync(exciteIm, W)
+
+    fig = plt.figure()
+    plt.plot(E)
+    plt.title("Energy plot of image 11")
+    plt.xlabel("Training steps")
+    plt.ylabel("Energy")
+    #fig.savefig("Energy_image_11.png")
+
+    X = [np.zeros(X[0].shape), np.zeros(X[0].shape), np.zeros(X[0].shape)]
+    W = np.random.randn(1024,1024)
+    recalledIms,E = recallAsync(exciteIm, W)
+    fig = plt.figure()
+    plt.plot(E)
+    plt.title("Energy plot of image 11 with random weights")
+    plt.xlabel("Training steps")
+    plt.ylabel("Energy")
+    #fig.savefig("Energy_image_11_random_weights.png")
+
+    W = np.random.randn(1024,1024)
+    W = (W + W.T)/2
+    recalledIms,E = recallAsync(exciteIm, W)
+    fig = plt.figure()
+    plt.plot(E)
+    plt.title("Energy plot of image 11 with random, symmetric weights")
+    plt.xlabel("Training steps")
+    plt.ylabel("Energy")
+    #fig.savefig("Energy_image_11_random_weights_symmetric.png")
+
+    plotIms(X, exciteIm, recalledIms, save=1, append="", path="random_weights_symmetric.mp4")
+
+def assignment3_4():
+    #add noise to images and try to recall
+    #how much noise can be added?
+    X = loadData('pict.dat') #loads 32*32x11
+    attractor = X[:3]
+
+    X_noise = []
+    W = compW(attractor, iter=1, N=1024)
+    noiseStep = 0.1
+    noOfImages = 3
+    labels = []
+    noOfIterations = 5
+
+    for j in range(noOfImages):
+        for i in range(int(1/noiseStep)):
+            X_noise.append(addNoise(X[j],noiseStep*(i+1)))
+            labels.append(j)
+
+    for i in range(noOfIterations):
+        X_noise = recallAll(X_noise,W)
+
+    recalledPattern = []
+    for i in range(len(X_noise)):
+        if (np.sum(X[0] - X_noise[i]) <= 0):
+            recalledPattern.append(0)
+        elif (np.sum(X[1] - X_noise[i]) <= 0):
+            recalledPattern.append(1)
+        elif (np.sum(X[2] - X_noise[i]) <= 0):
+            recalledPattern.append(2)
+        else:
+            recalledPattern.append(-1)
+    print(recalledPattern)
+
+    for i in range(3):
+        fig = plt.figure()
+        plt.imshow(np.array(X[i]).reshape(32,32))
+        fig.savefig("img{}.png".format(i))
+
+    N = int(1/noiseStep)
+    for i in range(noOfImages):
+        fig, ax = plt.subplots(2,5)
+
+        for n in range(N):
+            plt.subplot(2,5,n+1)
+            plt.imshow(np.array(X_noise[i*N + n]).reshape(32,32))
+            plt.title("noise: {}".format(round((n+1)*(noiseStep),2)))
+
+        fig.savefig("recalled_{}_noisy".format(i))
+
+def assignment3_5():
+    X = loadData('pict.dat') #loads 32*32x11
+
+    performance = []
+
+    for i in range(1,8):
+        W = compW(X[:i])
+        noOfRecalled = 0
+        for n in range(i+1):
+            x_temp = np.copy(X[n])
+            x_noise = addNoise(x_temp, 0.3)
+            x_temp = recall(x_noise,W)
+            if (np.sum(x_temp-X[n]) == 0):
+                noOfRecalled+=1
+        performance.append(noOfRecalled)
+    print(performance)
+
+def assignment3_5_2():
+    noOfUnits = 100
+    noOfPatterns = 300
+    X_rand = []
+    performance = []
+    total_performance = []
+
+    for i in range(noOfPatterns):
+        mask = list(sign(0.5+randn(300,100)))
+    print(len(X_rand))
+
+    for i in range(1,len(X_rand)+1):
+        W = compW(X_rand[:i])
+        #print(X_rand[:i])
+        noOfRecalled = 0
+        for n in range(0,i):
+            #print(n)
+            x_temp = np.copy(X_rand[n])
+            x_temp = recall(x_temp,W)
+            if (np.sum(x_temp-X_rand[n]) == 0):
+                noOfRecalled+=1
+        performance.append(noOfRecalled)
+
+    performance_norm = np.divide(performance, np.arange(noOfPatterns)+1)
+    print(performance)
+    print(performance_norm)
+
+    cat_forgetting_limit = noOfUnits*0.18
+
+    fig, ax = plt.subplots(2,1, figsize=(8, 9))
+
+    plt.subplot(2,1,1)
+    plt.plot(performance)
+    plt.axvline(x=cat_forgetting_limit, c='r')
+    plt.title("Number of patterns stored")
+    plt.ylabel("# of patterns recalled")
+    plt.xlabel("# of patterns learned")
+
+    plt.subplot(2,1,2)
+    plt.plot(performance_norm)
+    plt.axvline(x=cat_forgetting_limit, c='r')
+    plt.title("Number of patterns stored normalised on patters learned")
+    plt.ylabel("Fraction of patterns recalled")
+    plt.xlabel("# of patterns learned")
+    fig.savefig("recalled_patterns.png")
+
+def assignment3_5_2_extra():
+    noOfUnits = 100
+    noOfPatterns = 300
+
+    iterations = 100
+    total_performance = []
+    total_performance_norm = []
+
+    for m in range(iterations):
+        print(m)
+        performance = []
+        X_rand = []
+
+        for i in range(noOfPatterns):
+            mask = np.random.choice([1, -1], size=(noOfUnits,))
+            X_rand.append(mask)
+
+        for i in range(1,len(X_rand)+1):
+            W = compW(X_rand[:i])
+            #print(X_rand[:i])
+            noOfRecalled = 0
+            for n in range(0,i):
+                #print(n)
+                x_temp = np.copy(X_rand[n])
+                x_temp = recall(x_temp,W)
+                if (np.sum(x_temp-X_rand[n]) == 0):
+                    noOfRecalled+=1
+            performance.append(noOfRecalled)
+        total_performance.append(performance)
+
+    total_performance = np.sum(total_performance,0)/iterations
+    total_performance_norm = np.divide(total_performance,np.arange(noOfPatterns)+1)
+    cat_forgetting_limit = noOfUnits*0.18
+
+    fig, ax = plt.subplots(2,1, figsize=(8, 9))
+    fig.suptitle("Mean of {} runs".format(iterations))
+
+    plt.subplot(2,1,1)
+    plt.plot(total_performance)
+    plt.axvline(x=cat_forgetting_limit, c='r')
+    plt.title("Number of patterns stored")
+    plt.ylabel("# of patterns recalled")
+    plt.xlabel("# of patterns learned")
+
+    plt.subplot(2,1,2)
+    plt.plot(total_performance_norm)
+    plt.axvline(x=cat_forgetting_limit, c='r')
+    plt.title("Number of patterns stored normalised on patters learned")
+    plt.ylabel("Fraction of patterns recalled")
+    plt.xlabel("# of patterns learned")
+    fig.savefig("recalled_patterns_mean.png")
+
 
 if __name__ == '__main__':
     #assignment3_0()
     #assignment3_1()
-    assignment3_2()
+    #assignment3_2()
+    #assignment3_3()
+    #assignment3_4()
+    #assignment3_5()
+    #assignment3_5_2()
+    #assignment3_5_2_extra()
